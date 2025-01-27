04f8566199c9f5ba1a7abe8a05288e340092cdc4
ArtyomInc <artiom.game@gmail.com>
2025-01-27 16:18:24 +0100

    Fix image ratio calculation

--- utils/export-pdf.ts
+++ utils/export-pdf.ts
@@ -1,44 +1,44 @@
-import { PDFDocument } from "pdf-lib";
+import { PDFDocument } from 'pdf-lib'
 
 export default async function exportPDF(images: string[], scale: number) {
-  const imageBytesPromises = images.map(async (imageUrl) => {
-    const res = await fetch(imageUrl);
-    return await res.arrayBuffer();
-  });
+  const imageBytesPromises = images.map(async imageUrl => {
+    const res = await fetch(imageUrl)
+    return await res.arrayBuffer()
+  })
 
-  const imageBytes = await Promise.all(imageBytesPromises);
+  const imageBytes = await Promise.all(imageBytesPromises)
 
-  const pdfDoc = await PDFDocument.create();
+  const pdfDoc = await PDFDocument.create()
 
   for (const imageData of imageBytes) {
-    const uint8Array = new Uint8Array(imageData);
+    const uint8Array = new Uint8Array(imageData)
 
-    const isJpg = uint8Array[0] === 0xff && uint8Array[1] === 0xd8;
+    const isJpg = uint8Array[0] === 0xff && uint8Array[1] === 0xd8
 
-    let image;
+    let image
     if (isJpg) {
-      image = await pdfDoc.embedJpg(imageData);
+      image = await pdfDoc.embedJpg(imageData)
     } else {
-      image = await pdfDoc.embedPng(imageData);
+      image = await pdfDoc.embedPng(imageData)
     }
 
-    const imageDims = image.scale(scale);
-
-    const page = pdfDoc.addPage([1169, 827]);
+    const page = pdfDoc.addPage([1169, 827])
+    const calculatedScale = page.getWidth() / image.width
+    const imageDims = image.scale(calculatedScale)
 
     page.drawImage(image, {
       height: imageDims.height,
       width: imageDims.width,
       x: page.getWidth() / 2 - imageDims.width / 2,
-      y: page.getHeight() / 2 - imageDims.height / 2,
-    });
+      y: page.getHeight() / 2 - imageDims.height / 2
+    })
   }
 
-  const pdfBytes = await pdfDoc.save();
+  const pdfBytes = await pdfDoc.save()
 
-  const blob = new Blob([pdfBytes], { type: "application/pdf" });
-  const link = document.createElement("a");
-  link.href = URL.createObjectURL(blob);
-  link.download = "document.pdf";
-  link.click();
+  const blob = new Blob([pdfBytes], { type: 'application/pdf' })
+  const link = document.createElement('a')
+  link.href = URL.createObjectURL(blob)
+  link.download = 'document.pdf'
+  link.click()
 }
