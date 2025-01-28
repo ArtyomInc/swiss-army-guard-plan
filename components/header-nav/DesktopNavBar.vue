47e045945bbe014b845a3ef72f52704a4795d751
ArtyomInc <artiom.game@gmail.com>
2025-01-28 13:51:09 +0100

    Fix generator button hover style

--- components/header-nav/DesktopNavBar.vue
+++ components/header-nav/DesktopNavBar.vue
@@ -1,7 +1,11 @@
 <template>
   <nav class="flex h-full items-center justify-between">
     <div class="p-2 h-full">
-      <NuxtImg class="h-full blur-none" src="/favicon.svg" alt="ArtyomInc logo" />
+      <NuxtImg
+        class="h-full blur-none"
+        src="/favicon.svg"
+        alt="ArtyomInc logo"
+      />
     </div>
     <div class="flex items-center gap-2">
       <template v-for="item in props.links" :key="item.name">
@@ -9,7 +13,12 @@
           {{ item.name }}
         </a>
       </template>
-      <Link variant="outline" size="icon" href="https://github.com/ArtyomInc" target="_blank">
+      <Link
+        variant="outline"
+        size="icon"
+        href="https://github.com/ArtyomInc"
+        target="_blank"
+      >
         <Icon name="mdi:github" size="20" />
       </Link>
       <SwitchTheme />
@@ -18,15 +27,15 @@
 </template>
 
 <script setup lang="ts">
-import { Link } from '@/ui/link'
-import { navigationMenuTriggerStyle } from '@/ui/navigation-menu'
+import { Link } from "@/ui/link";
+import { navigationMenuTriggerStyle } from "@/ui/navigation-menu";
 
-import SwitchTheme from './theme-switch/SwitchTheme.vue'
+import SwitchTheme from "./theme-switch/SwitchTheme.vue";
 
 const props = defineProps<{
   links: {
-    name: string
-    path: string
-  }[]
-}>()
+    name: string;
+    path: string;
+  }[];
+}>();
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,31 +2,25 @@
   <div class="flex justify-center items-center">
     <div class="flex flex-col gap-10 justify-center max-w-screen-md mt-[10vh]">
       <div class="relative flex justify-center items-center">
-        <div
-          class="absolute from-violet-400 to-green-400 bg-gradient-to-br w-full h-[30vh] blur-3xl opacity-50"
-        />
-        <h1 class="text-5xl z-10 text-center">
-          Le générateur open-source de plan de garde pour militaire
-        </h1>
+        <div class="absolute from-violet-400 to-green-400 bg-gradient-to-br w-full h-[30vh] blur-3xl opacity-50" />
+        <h1 class="text-5xl z-10 text-center">Le générateur open-source de plan de garde pour militaire</h1>
       </div>
       <div class="flex justify-center items-center">
         <p class="text-center text-xl">
           Bienvenue sur l'outil personnalisable et open source de
-          <strong>génération de plans de garde</strong> pour les militaires
-          suisses.
+          <strong>génération de plans de garde</strong> pour les militaires suisses.
         </p>
       </div>
       <div class="flex justify-center items-center">
         <p class="text-center text-foreground/70">
-          Cet outil vous permet de planifier des binômes de soldats par tranche
-          horaire, avec plusieurs types d'affectation tels que Externo, Zuko,
-          garde, etc. Très flexible, il offre la possibilité d'ajouter des
+          Cet outil vous permet de planifier des binômes de soldats par tranche horaire, avec plusieurs types
+          d'affectation tels que Externo, Zuko, garde, etc. Très flexible, il offre la possibilité d'ajouter des
           tranches horaires et des binômes selon vos besoins.
         </p>
       </div>
       <div class="flex justify-center items-center flex-col md:flex-row gap-3">
         <a
-          class="flex items-center gap-2 bg-gradient-to-r from-violet-500 to-green-400 px-4 justify-center text-white rounded-lg py-2"
+          class="flex items-center hover:opacity-90 gap-2 bg-gradient-to-r from-violet-500 to-green-400 px-4 justify-center text-white rounded-lg py-2"
           href="/guard-plan"
           >Utiliser le générateur
           <Icon name="lucide:bar-chart-horizontal-big" size="20" />
