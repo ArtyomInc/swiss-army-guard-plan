b45e5c3aca6030e84f72b221e7df18e664c7931b
ArtyomInc <artiom.game@gmail.com>
2025-02-06 18:52:02 +0100

    Fix typo

--- pages/guard-plan.vue
+++ pages/guard-plan.vue
@@ -2,10 +2,7 @@
   <Card class="mt-[5vh] mx-1 md:mx-5">
     <CardHeader>
       <CardTitle>Créer les plans</CardTitle>
-      <CardDescription
-        >Renseignez les champs pour ensuite générer les plan par
-        jours</CardDescription
-      >
+      <CardDescription>Renseignez les champs pour ensuite générer les plans par jours</CardDescription>
     </CardHeader>
     <CardContent>
       <UserForm v-model="userField" />
@@ -15,18 +12,12 @@
 </template>
 
 <script setup lang="ts">
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/ui/card'
 
-import type { userForm } from "~/interfaces";
+import type { userForm } from '~/interfaces'
 
-import GuardPlan from "./guard-plan/GuardPlan.vue";
-import UserForm from "./guard-plan/UserForm.vue";
+import GuardPlan from './guard-plan/GuardPlan.vue'
+import UserForm from './guard-plan/UserForm.vue'
 
-const userField = ref<userForm>();
+const userField = ref<userForm>()
 </script>
